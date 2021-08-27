bf524ddb43b7a7dbc9d8fc07e9b933ae16d7797e
mina <takhminam@gmail.com>
2021-08-28 00:11:50 +0300

    Add addtodo, and EventListener to the index file

--- src/index.js
+++ src/index.js
@@ -1,37 +1,63 @@
 import './style.css';
+import {
+  CHECK, UNCHECK, LINE_THROUGH, TRASH_ICON, listToDo,
+  completeToDo
+} from './completeToDo.js';
 
-const button = document.querySelector('.fa-reply');
-button.addEventListener('click', () => {
-  console.log('Lets drink coffee');
+// const LIST_KEY = 'task.listToDo';
+// listToDo = JSON.parse(localStorage.getItem(LIST_KEY)) || [];
+
+// add item to the local storage
+function save() {
+  localStorage.setItem(LIST_KEY, JSON.stringify(listToDo));
+}
+
+const list = document.getElementById('todo-list');
+const input = document.getElementById('input');
+
+const ELIPSIS = 'fa-ellipsis-v';
+let index = 0;
+const complete = false;
+
+function addToDo(description, index, complete) {
+  input.value = '';
+  const DONE = complete ? CHECK : UNCHECK;
+  const LINE = complete ? LINE_THROUGH : '';
+  const TRASH = complete ? TRASH_ICON : ELIPSIS;
+  const item = `<li class="item">
+        <i class="far ${DONE} co" job="complete" id="${index}"></i>
+        <p class="text ${LINE}">${description}</p>
+        <i class="trash fas ${TRASH} " job="delete" id="${index}"></i>
+    </li>
+    `;
+  const position = 'beforeend';
+  list.insertAdjacentHTML(position, item); 
+   completeToDo(listToDo[index]);
+}
+
+document.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    const inputValue = input.value;
+    if (inputValue) {
+      addToDo(inputValue, index, complete);
+      listToDo.push({
+        inputValue,
+        index,
+        complete: false,
+      });
+      // save();
+      index += 1;
+    }
+  }
 });
-const listToDo = [
-  {
-    completed: true,
-    description: 'Drink a coffee',
-    index: 0,
-  },
-  {
-    completed: false,
-    description: 'Do JavaScript project',
-    index: 1,
-  },
-];
-const list = document.querySelector('#todo-list');
-listToDo.forEach((todoItem) => {
-  const unorderedList = document.createElement('li');
 
-  const checkButton = document.createElement('div');
-  checkButton.id = 'checkList';
-  checkButton.innerHTML = '<i class="far fa-square"></i>';
-  unorderedList.append(checkButton);
 
-  const newToDo = document.createElement('p');
-  newToDo.innerText = todoItem.description;
-  unorderedList.append(newToDo);
 
-  const moveButton = document.createElement('div');
-  moveButton.id = 'moveList';
-  moveButton.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
-  unorderedList.append(moveButton);
-  list.append(unorderedList);
+list.addEventListener('click', (event) => {
+  const element = event.target;
+  const elementJob = element.attributes.job.value;
+  if (elementJob === 'complete') {
+    completeToDo(element);
+  }
+  // save();
 });
